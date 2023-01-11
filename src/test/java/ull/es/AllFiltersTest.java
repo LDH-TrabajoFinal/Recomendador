7180eb3c577613823a11f7e20b1b62ae2ba0dd2e
alu0101330600 <65622828+jordidogo@users.noreply.github.com>
2023-01-11 15:46:22 +0000

    Añadidas pruebas AllFilter

---
+++ src/test/java/ull/es/AllFiltersTest.java
@@ -0,0 +1,32 @@
+package ull.es;
+
+import ull.es.AllFilters;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AllFiltersTest {
+
+	@Test
+	void testAllFilters() {
+	}
+
+	@Test
+	void testAddFilter() {
+		AllFilters allFilters = new AllFilters();
+		allFilters.addFilter(new AllFilters());
+		assertEquals(1, allFilters.filters.size());
+	}
+
+	@Test
+	void testSatisfies() {
+		assertEquals(true, new AllFilters().satisfies("1"));
+	}
+
+	@Test
+	void testToString() {
+		assertEquals("ull.es.AllFilters@332f25c8", new AllFilters().toString());
+	}
+
+}
